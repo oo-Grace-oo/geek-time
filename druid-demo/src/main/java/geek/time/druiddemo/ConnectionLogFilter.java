b0727727056d5ba0b3d315aaa09084582fbabaaf
xiangyang.liu <Lxy970603>
2022-01-04 14:56:21 +0800

    druid-demo

---
+++ druid-demo/src/main/java/geek/time/druiddemo/ConnectionLogFilter.java
@@ -0,0 +1,32 @@
+package geek.time.druiddemo;
+
+
+import com.alibaba.druid.filter.FilterChain;
+import com.alibaba.druid.filter.FilterEventAdapter;
+import com.alibaba.druid.proxy.jdbc.ConnectionProxy;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Properties;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author User
+ * @date 2022/01/04
+ */
+@Slf4j
+public class ConnectionLogFilter extends FilterEventAdapter {
+
+    @Override
+    public void connection_connectBefore(FilterChain chain, Properties info) {
+        log.info("BEFORE CONNECTION!");
+    }
+
+    @Override
+    public void connection_connectAfter(ConnectionProxy connection) {
+        log.info("AFTER CONNECTION!");
+    }
+}
+
---
+++ druid-demo/src/test/java/geek/time/druiddemo/DruidDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package geek.time.druiddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DruidDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
